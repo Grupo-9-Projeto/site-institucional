4500ce7b2bab9f70f2b742b44cfa0aef826b11b1
JotaVSRodrigues <joao.rdg119@gmail.com>
2026-08-16 21:40:09 -0300

    fetch structure + verifyField function

--- src/script.js
+++ src/script.js
@@ -1,10 +1,18 @@
 
 function registerUser() {
-    const name;
-    const email;
-    const passwordHash;
-    const token;
+    let name;
+    let email;
+    let passwordHash;
+    let token;
     
+    const verifyFieldParam = { name, email, passwordHash, token }
+
+    try {
+        verifyField(verifyFieldParam)    
+        console.log("Todos os campos estão válidos!");
+    } catch {
+         console.error("Erro de validação:", error);
+    }
 
     fetch("/usuario/cadastrar", {
         method: "POST"  ,
@@ -33,4 +41,29 @@ function registerUser() {
 
 function authUser() {
     
+}
+
+function verifyField(fieldsObj) {
+    for (const [fieldName, fieldValue] of Object.entries(fieldsObj)) {
+
+        if (!fieldValue || String(fieldValue).trim().length === 0) {
+            throw `O campo [${fieldName}] não pode estar vazio.`
+        }
+
+        const valueStr = String(fieldValue).trim()
+
+        if (fieldName === "email") {
+            const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            if (!regexEmail.test(valueStr)) throw "O email inserido é inválido"
+        }
+
+        if (fieldName === "passwordHash") {
+            const regexPassword = /^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d).{8,}$/
+            if (!regexPassword.test(valueStr)) {
+                throw "A senha deve ter pelo menos 8 caracteres, 1 letra maiúscula, 1 número e 1 caractere especial."
+            }
+        }
+    }
+
+    return true
 }
